ed32753c2531c47150a6e6680a4ab14353d9b1e3
Hans-Märten Liiu <70939468+hansm4543@users.noreply.github.com>
2021-04-27 11:18:53 +0300

    Muutused tehtud

--- w11_proovikontrolltoo/prooviKT/JoogiTestid.java
+++ w11_proovikontrolltoo/prooviKT/JoogiTestid.java
@@ -7,6 +7,7 @@ public class JoogiTestid {
 	Joogivaat v1 = new Joogivaat(100.0, 25.0);
 	Joogivaat v2 = new Joogivaat(50.0, 5.0);
 	Joogivaat v3 = new Joogivaat(50.0, 50.0);
+	JoogiPudeliteKast k1 = new JoogiPudeliteKast("puidust", 1000, 1000, 36);
     
     @Test
 	public void esimene(){
@@ -42,6 +43,15 @@ public class JoogiTestid {
 	@Test
 	public void kontroll7(){
 		assertEquals("jook on ostas ja täidetud sai " + 5 + " pudelit", v3.taidaPudelid(10, p2));
+	}
+	@Test
+	public void kontroll8(){
+		assertEquals("Pudelid on täidetud ja kast on t2is", k1.villipudelidkastis(v3, p2));
+	}
+	@Test
+	public void kontroll9(){
+		p2.lisaJook(j2);
+		assertEquals("198.0", k1.kastispudeliteomahind(p2));
 	}
 
 	@Test
